cc86fd8a4df0cbca0b9defdd9dca04c38ac9854c
Ilkinur Keremli <ilkinur.keremli@kibrit.tech>
2024-10-16 21:36:15 +0400

    delete width changed

--- TaskManagerVue/src/components/dashboard/Tasks.vue
+++ TaskManagerVue/src/components/dashboard/Tasks.vue
@@ -95,7 +95,7 @@
       :currentPage="currentPage"
       @pagechanged="onPageChange"
     />
-    <modal name="delete" :widht="800" height="auto" styles="background-color: transparent;">
+    <modal name="delete" height="auto" styles="background-color: transparent; width:500px!important">
       <div class="modal-dialog modal-dialog-centered" style="margin: 0!important; width: 100%!important;">
             <div class="modal-content">
                 <div class="modal-header">
